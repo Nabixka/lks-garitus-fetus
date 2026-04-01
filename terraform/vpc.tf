4be34da826546207cf87f44b6faf58c3530c2a88
nabixka <nabixka05@gmail.com>
2026-04-01 20:33:40 +0700

    VPC Terraform

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,6 @@
 # VPC
 resource "aws_vpc" "lks_pm_vpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block       = "10.100.0.0/16"
   instance_tenancy = "default"
 
   tags = {
@@ -8,9 +8,12 @@ resource "aws_vpc" "lks_pm_vpc" {
   }
 }
 
+# Subnet
 resource "aws_subnet" "lks_pm_public_subnet_1a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.100.0.0/24 "
+  vpc_id     = aws_vpc.lks_pm_vpc.id
+  availability_zone = "us-east-1a"
+  cidr_block = "10.100.0.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "lks-pm-public-subnet-1a"
@@ -18,8 +21,10 @@ resource "aws_subnet" "lks_pm_public_subnet_1a" {
 }
 
 resource "aws_subnet" "lks_pm_public_subnet_1b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.100.1.0/24 "
+  vpc_id     = aws_vpc.lks_pm_vpc.id
+  availability_zone = "us-east-1b"
+  cidr_block = "10.100.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "lks-pm-public-subnet-1b"
@@ -27,8 +32,9 @@ resource "aws_subnet" "lks_pm_public_subnet_1b" {
 }
 
 resource "aws_subnet" "lks_pm_private_subnet_1a" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.100.10.0/24 "
+  vpc_id     = aws_vpc.lks_pm_vpc.id
+  availability_zone = "us-east-1a"
+  cidr_block = "10.100.10.0/24"
 
   tags = {
     Name = "lks-pm-private-subnet-1a"
@@ -36,14 +42,21 @@ resource "aws_subnet" "lks_pm_private_subnet_1a" {
 }
 
 resource "aws_subnet" "lks_pm_private_subnet_1b" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.100.11.0/24 "
+  vpc_id     = aws_vpc.lks_pm_vpc.id
+  availability_zone = "us-east-1b"
+  cidr_block = "10.100.11.0/24"
 
   tags = {
     Name = "lks-pm-private-subnet-1b"
   }
 }
 
+# EIP
+resource "aws_eip" "lks_nat_eip" {
+  domain = "vpc"
+}
+
+# IGW & NAT
 resource "aws_internet_gateway" "lks_pm_igw" {
   vpc_id = aws_vpc.lks_pm_vpc.id
 
@@ -52,6 +65,18 @@ resource "aws_internet_gateway" "lks_pm_igw" {
   }
 }
 
+resource "aws_nat_gateway" "lks_pm_natgw" {
+  allocation_id = aws_eip.lks_nat_eip.id
+  subnet_id     = aws_subnet.lks_pm_public_subnet_1a.id
+
+  tags = {
+    Name = "lks-pm-natgw"
+  }
+
+  depends_on = [aws_internet_gateway.lks_pm_igw]
+}
+
+# RTB
 resource "aws_route_table" "lks_pm_public_rt" {
   vpc_id = aws_vpc.lks_pm_vpc.id
 
@@ -65,6 +90,19 @@ resource "aws_route_table" "lks_pm_public_rt" {
   }
 }
 
+resource "aws_route_table" "lks_pm_private_rt" {
+  vpc_id = aws_vpc.lks_pm_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.lks_pm_natgw.id
+  }
+
+  tags = {
+    Name = "lks-pm-private-rt"
+  }
+}
+
 resource "aws_route_table_association" "lks_pm_public_1a_association" {
   subnet_id      = aws_subnet.lks_pm_public_subnet_1a.id
   route_table_id = aws_route_table.lks_pm_public_rt.id
@@ -73,4 +111,14 @@ resource "aws_route_table_association" "lks_pm_public_1a_association" {
 resource "aws_route_table_association" "lks_pm_public_1b_association" {
   subnet_id      = aws_subnet.lks_pm_public_subnet_1b.id
   route_table_id = aws_route_table.lks_pm_public_rt.id
+}
+
+resource "aws_route_table_association" "lks_pm_private_1a_association" {
+  subnet_id      = aws_subnet.lks_pm_private_subnet_1a.id
+  route_table_id = aws_route_table.lks_pm_private_rt.id
+}
+
+resource "aws_route_table_association" "lks_pm_private_1b_association" {
+  subnet_id      = aws_subnet.lks_pm_private_subnet_1b.id
+  route_table_id = aws_route_table.lks_pm_private_rt.id
 }
